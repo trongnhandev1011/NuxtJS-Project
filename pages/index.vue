3564f8f6739d5a2b3a001a5fdc15168bf27d6eef
unknown <nhantrongho2002@gmail.com>
2022-07-18 12:21:44 +0700

    fix error in pass props through nuxt child

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,20 @@
 <template>
-  <home-page-template />
+  <home-page-template :searchedCardList="searchedCardList" />
 </template>
 
 <script>
 import HomePageTemplate from "../components/L4_Template/L4_03_HomePageTemplate/HomePageTemplate.vue";
 export default {
   name: "HomePage",
+  props: {
+    searchedCardList: Array,
+  },
+  setup(props) {
+    let searchedCardList = props.searchedCardList;
+    return {
+      searchedCardList,
+    };
+  },
   components: { HomePageTemplate },
 };
 </script>
